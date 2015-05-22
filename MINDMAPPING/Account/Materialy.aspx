e75d7981be9e54a26781c4b9e0a5fe7b40b06322
Piotr Opiełka <piotrek.opielka@gmail.com>
2015-05-22 16:13:24 +0200

    Wyświetlanie przedmiotów zrobione

--- MINDMAPPING/Account/Materialy.aspx
+++ MINDMAPPING/Account/Materialy.aspx
@@ -4,5 +4,12 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:GridView ID="GridView1" runat="server" Width="944px"></asp:GridView>
+    <div><asp:GridView ID="GridPrzedmioty"  HorizontalAlign="Left" runat="server"  AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField DataField="Nazwa"  HeaderText ="Nazwa przedmiotu"/>
+            <asp:ButtonField ButtonType="Button"  Text="Wyświetl" ControlStyle-CssClass="contact" />
+        </Columns>
+        </asp:GridView></div>
+    
+    <asp:GridView ID="GridMaterialy" runat="server" Width="400px"></asp:GridView>
 </asp:Content>
--- MINDMAPPING/Account/Materialy.aspx.cs
+++ MINDMAPPING/Account/Materialy.aspx.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace PlanZajec
 {
     public partial class Materialy : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (!Page.IsPostBack)
+            {
+                BindGridPrzedmioty();
+            }
+        }
+
+        private void BindGridPrzedmioty()
+        {
+           DataTable dataTable = new DataTable();
+           SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["mywindowshosting"].ConnectionString);
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Przedmioty";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                dataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    GridPrzedmioty.DataSource = dataTable;
+                    GridPrzedmioty.DataBind();
+                }
+            }
+            catch (Exception)
+            {
+                {
+                }
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
 
         }
     }
