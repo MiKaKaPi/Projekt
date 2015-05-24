f0bc5811ce6e57e2e8ac06d5e1f624544ba3c897
Piotr Opiełka <piotrek.opielka@gmail.com>
2015-05-24 17:06:55 +0200

    powiązanie przedmiotów z materiałami; do zrobienia pobieranie plików

--- MINDMAPPING/Account/Materialy.aspx
+++ MINDMAPPING/Account/Materialy.aspx
@@ -4,12 +4,20 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
-    <div><asp:GridView ID="GridPrzedmioty"  HorizontalAlign="Left" runat="server"  AutoGenerateColumns="false">
+    <div><asp:GridView ID="GridPrzedmioty"  HorizontalAlign="Left" runat="server"  AutoGenerateColumns="false" OnRowCommand="GridPrzedmioty_OnRowCommand">
         <Columns>
             <asp:BoundField DataField="Nazwa"  HeaderText ="Nazwa przedmiotu"/>
             <asp:ButtonField ButtonType="Button"  Text="Wyświetl" ControlStyle-CssClass="contact" />
         </Columns>
-        </asp:GridView></div>
+        </asp:GridView>
+
+    </div>
     
-    <asp:GridView ID="GridMaterialy" runat="server" Width="400px"></asp:GridView>
+    <asp:GridView ID="GridMaterialy" runat="server" Width="400px" AutoGenerateColumns="False">
+        <Columns>
+            <asp:BoundField DataField="Nazwa" HeaderText="Nazwa"/>
+            <asp:BoundField DataField="Rozszerzenie" HeaderText="Rozszerzenie"/>
+            <asp:BoundField DataField="Opis" HeaderText="Opis"/>
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- MINDMAPPING/Account/Materialy.aspx.cs
+++ MINDMAPPING/Account/Materialy.aspx.cs
@@ -13,18 +13,20 @@ namespace PlanZajec
     public partial class Materialy : System.Web.UI.Page
     {
         
+        private DataTable _dataTablePrzedmioty;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             
-            if (!Page.IsPostBack)
-            {
+            
                 BindGridPrzedmioty();
-            }
+            
         }
 
         private void BindGridPrzedmioty()
         {
-           DataTable dataTable = new DataTable();
+           
+           _dataTablePrzedmioty = new DataTable();
            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["mywindowshosting"].ConnectionString);
 
             try
@@ -33,10 +35,10 @@ namespace PlanZajec
                 string query = "SELECT * FROM Przedmioty";
                 SqlCommand cmd = new SqlCommand(query, connection);
                 SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
-                dataAdapter.Fill(dataTable);
-                if (dataTable.Rows.Count > 0)
+                dataAdapter.Fill(_dataTablePrzedmioty);
+                if (_dataTablePrzedmioty.Rows.Count > 0)
                 {
-                    GridPrzedmioty.DataSource = dataTable;
+                    GridPrzedmioty.DataSource = _dataTablePrzedmioty;
                     GridPrzedmioty.DataBind();
                 }
             }
@@ -50,6 +52,56 @@ namespace PlanZajec
             {
                 connection.Close();
             }
+
+        }
+
+        private void BindGridMaterialyById(int subjectId)
+        {
+            
+            DataTable dataTable = new DataTable();
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["mywindowshosting"].ConnectionString);
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Pliki where IdPrzedmiotu = @id";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = query;
+                SqlParameter parameter = new SqlParameter("@id", subjectId);
+                cmd.Parameters.Add(parameter);
+                dataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    GridMaterialy.DataSource = dataTable;
+                    GridMaterialy.DataBind();
+                }
+                else
+                {
+                    GridMaterialy.DataSource = null;
+                    GridMaterialy.DataBind();
+                
+                }
+            }
+            catch (Exception)
+            {
+                {
+                }
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        protected void GridPrzedmioty_OnRowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int numberOfClickedRow = Int32.Parse(e.CommandArgument.ToString());
+            const int iterFromTable = 0;
+            int subjectId = Int32.Parse(_dataTablePrzedmioty.Rows[numberOfClickedRow].ItemArray[iterFromTable].ToString());
+            BindGridMaterialyById(subjectId);
 
         }
     }
